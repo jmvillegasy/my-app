13d1ab1197731e387a883c560827b1ed5afe4ced
Juan Villegas <juanvillegasy@hotmail.com>
2020-09-26 13:04:48 -0500

    tercer commit

---
+++ my-app/src/app/dish/dish.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'rw-dish',
+    templateUrl: './dish.component.html',
+    styleUrls: ['./dish.component.scss']
+})
