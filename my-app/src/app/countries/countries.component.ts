2a5459763d942642965d803fcbaae31825d3b065
Juan Villegas <juanvillegasy@hotmail.com>
2020-10-16 16:42:55 -0500

    tercera clase uso de pipes en ts

--- my-app/src/app/countries/countries.component.ts
+++ my-app/src/app/countries/countries.component.ts
@@ -1,3 +1,4 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Country } from '@rw/models';
 import * as countriesJson from './../../assets/countries.json';
@@ -5,14 +6,17 @@ import * as countriesJson from './../../assets/countries.json';
 @Component({
   selector: 'rw-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
+  providers: [UpperCasePipe]
 })
 export class CountriesComponent implements OnInit {
   selected: Country;
   countries: Country[];
-  constructor() {
+  birthday = new Date();
+  total = 1223456.555551;
+  constructor(private upperCasePipe: UpperCasePipe) {
     //console.log(countries[10].capital);
-    this.selected = countriesJson[10];
+    this.selected = countriesJson[2];
     //this.selected = countriesJson;
     /*this.selected = this.countries.filter(
       (country: Country) => (country.name = 'Peru')
@@ -21,6 +25,13 @@ export class CountriesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  send(name:string):void{
+    console.log('Antes:::',name);
+    name = this.upperCasePipe.transform(name);
+    //name = name.toUpperCase();
+    console.log('Despues:::',name);
   }
 
 }
